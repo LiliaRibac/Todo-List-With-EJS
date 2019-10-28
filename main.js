30d4e663d31c0d934de5675ab9e376d8700ad66d
LiliaRibac <ribac.lilia@gmai.com>
2019-10-28 13:27:13 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+
+   let day= date.getDate();
+  res.render("lists", {
+    listTitle: day,
+    newListItems: items
+
+  })
+  // console.log(newListItems)
+})
+
+app.post("/", function (req, res) {
+
+  let item = req.body.newItem
+
+  if(req.body.lists === "Work") {
+  workItems.push(item)
+  res.redirect("/work")
+}else{
+  items.push(item)
+  res.redirect("/")
+}
+})
+
+
+
+
+app.post("/work", function(req,res){
+
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work")
+})
+
+app.get("/work",function(req,res){
+  res.render("lists", {listTitle:"Work List", newListItems:workItems})
+  })
+
+app.get("/about", function (req,res) {
+  res.render("about")
+  })
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+})
