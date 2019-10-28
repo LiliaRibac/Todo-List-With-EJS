c85f1c99b40d902a7c6e897b5c414394f6b78019
LiliaRibac <ribac.lilia@gmai.com>
2019-10-28 14:46:20 -0700

    add  Items in main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose")
+
+// const date = require(__dirname + "/date.js")
 
 const app = express();
 
@@ -13,12 +15,45 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true
+});
+
+const itemsSchema = {
+  name: String
+}
+var Item = mongoose.model("Item", itemsSchema)
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+  name: "Hit the + button to add a new  item."
+})
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3]
+
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log("Successfully saved default items to DB.")
+  }
+})
+
+
 app.get("/", function (req, res) {
 
-   let day= date.getDate();
+
   res.render("lists", {
-    listTitle: day,
-    newListItems: items
+    listTitle: "Today",
+     newListItems: items
 
   })
   // console.log(newListItems)
@@ -28,32 +63,35 @@ app.post("/", function (req, res) {
 
   let item = req.body.newItem
 
-  if(req.body.lists === "Work") {
-  workItems.push(item)
-  res.redirect("/work")
-}else{
-  items.push(item)
-  res.redirect("/")
-}
+  if (req.body.lists === "Work") {
+    workItems.push(item)
+    res.redirect("/work")
+  } else {
+    items.push(item)
+    res.redirect("/")
+  }
 })
 
 
 
 
-app.post("/work", function(req,res){
+app.post("/work", function (req, res) {
 
   let item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work")
 })
 
-app.get("/work",function(req,res){
-  res.render("lists", {listTitle:"Work List", newListItems:workItems})
+app.get("/work", function (req, res) {
+  res.render("lists", {
+    listTitle: "Work List",
+    newListItems: workItems
   })
+})
 
-app.get("/about", function (req,res) {
+app.get("/about", function (req, res) {
   res.render("about")
-  })
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
